aec015a38e24939bd81128025a2a99f6827ba8f8
Michael Hansen <hansen.mike@gmail.com>
2020-10-18 10:17:48 -0400

    Use venv python

--- voice-nl-rdh/run.sh
+++ voice-nl-rdh/run.sh
@@ -3,9 +3,8 @@
 APP_DIR='/app'
 
 cd "${APP_DIR}"
-source .venv/bin/python3
 
-python3 -m larynx serve \
+.venv/bin/python3 -m larynx serve \
         --model 'voice/tts/nl-rdh_tts-v1.pth.tar' \
         --vocoder-model 'voice/vocoder/nl-rdh_vocoder-v1.pth.tar' \
         --cache-dir '/cache'
